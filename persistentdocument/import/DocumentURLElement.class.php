f74dac35eeebb78a15ccd08bee8abf358291b594
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-03-04 15:22:56 +0000

    [NEW] ajout de l'element de script documenturl qui permet de definir de la réécriture d'url dans les scripts

---
+++ persistentdocument/import/DocumentURLElement.class.php
@@ -0,0 +1,59 @@
+<?php
+class website_DocumentURLElement extends import_ScriptBaseElement
+{
+	public function process()
+	{
+		$parent = $this->getParent();
+		$pp = f_persistentdocument_PersistentProvider::getInstance();
+		
+		if ($parent instanceof import_ScriptDocumentElement)
+		{
+			$document = $parent->getPersistentDocument();
+			$documentId = $document->getId();
+			
+			$ds = $document->getDocumentService();
+			$websiteId = $ds->getWebsiteId($document); 
+			$url = $this->attributes['url'];
+			
+			$lang = isset($this->attributes['lang']) ? $this->attributes['lang'] : $document->getLang();
+			
+			$redirection = isset($this->attributes['redirection']) && $this->attributes['redirection'] === 'true'; 
+			if ($redirection)
+			{
+				$to_url = $this->getDocumentURL($document, $lang);
+				if ($to_url !== null)
+				{
+					$type = (isset($this->attributes['permanently']) &&  $this->attributes['permanently'] === 'true') ? 301 : 302;
+					$pp->setUrlRewriting($documentId, $lang, $websiteId, $url, $to_url, $type);
+				}
+				else
+				{
+					Framework::warn("Document " . $document->getLabel() . ", $documentId has destination URL for redirection $url");
+				}
+			}
+			else
+			{
+				$ds->setUrlRewriting($document, $lang, $url);
+				$pp->setUrlRewriting($documentId, $lang, $websiteId, $url, null, 200);
+			}	
+		}
+	}
+	
+	private function getDocumentURL($document, $lang)
+	{
+		try 
+		{
+			$url = LinkHelper::getDocumentUrl($document, $lang, array(), false);
+			$matches = array();
+			if (preg_match('/^https?:\/\/([^\/]*)(\/'.$lang.')?(\/.*)$/', $url, $matches))
+			{
+				return $matches[3];
+			}
+		}
+		catch (Exception $e)
+		{
+			Framework::exception($e);
+		}
+		return null;
+	}
+}
