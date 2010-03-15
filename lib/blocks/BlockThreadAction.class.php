64b8cfdc7770881b0f425c828754bf6299b3feb9
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-03-15 14:05:26 +0000

    [NEW] Réécriture du bloc 'modules_website_thread' [PATCH 0312] Si le projet surcharge 'Website-Block-Thread-Success.all.all.html', le patch convertie les references à 'modules_website_thread' en 'modules_website_threadOld'

--- lib/blocks/BlockThreadAction.class.php
+++ lib/blocks/BlockThreadAction.class.php
@@ -8,31 +8,22 @@ class website_BlockThreadAction extends website_BlockAction
 	 */
 	public function getCacheDependencies()
 	{
-		return array("modules_website/menu",
-		 "modules_website/menu",
-		 "modules_website/menuitem",
-		 "modules_website/menuitemdocument",
-		 "modules_website/menuitemfunction",
-		 "modules_website/menuitemtext",
-		 "modules_website/page",
-		 "modules_website/pagegroup",
-		 "modules_website/pageexternal",
-		 "modules_website/pagereference",
-		 "modules_website/pageversion",
-		 "modules_website/topic",
-		 "modules_website/website");
+		return array("modules_website/page", "modules_website/pagegroup", 
+			"modules_website/pagereference", "modules_website/pageversion", 
+			"modules_website/topic", "modules_website/systemtopic", "modules_website/website");
 	}
-
+	
 	/**
 	 * @param website_BlockActionRequest $request
 	 * @return array<mixed>
 	 */
 	public function getCacheKeyParameters($request)
 	{
-		return array("context->id" => $this->getPage()->getId(),
+		return array("context->id" => $this->getPage()->getId(), 
+			"context->label" => $this->getPage()->getNavigationtitle(), 
 			"lang->id" => RequestContext::getInstance()->getLang());
 	}
-
+	
 	/**
 	 * @see website_BlockAction::execute()
 	 *
@@ -42,40 +33,43 @@ class website_BlockThreadAction extends website_BlockAction
 	 */
 	function execute($request, $response)
 	{
-		$page = $this->getPage()->getPersistentPage();		
-		$ws = website_WebsiteModuleService::getInstance();
-		$currentWebsite = $ws->getCurrentWebsite();
-
-		if ($currentWebsite && $currentWebsite->getIndexPage() && $page)
+		$pageContext = $this->getPage();
+		$pageDocument = $pageContext->getPersistentPage();
+		$breadcrumb = array();		
+		
+		if (! $pageDocument->getIsHomePage())
 		{
-			if ($currentWebsite->getIndexPage()->getId() == $page->getId())
+			foreach ($pageContext->getAncestorIds() as $ancestorId)
 			{
-				$includeCurrentDocument = false;
+				$ancestor = DocumentHelper::getDocumentInstance($ancestorId);
+				if (! $ancestor->isPublished())
+				{
+					continue;
+				}
+				
+				if ($ancestor instanceof website_persistentdocument_website)
+				{
+					$siteUrl = LinkHelper::getDocumentUrl($ancestor);
+					$homeTitle = f_Locale::translate('&modules.website.frontoffice.thread.Homepage-href-name;');
+					
+					$breadcrumb[] = array('navigationtitle' => $homeTitle, 'href' => $siteUrl, 'class' => 'first');
+					$pageContext->addLink("home", "text/html", $siteUrl, $homeTitle);
+				}
+				else if ($ancestor instanceof website_persistentdocument_topic)
+				{
+					if ($ancestor->getNavigationVisibility() != 1)
+					{
+						continue;
+					}
+					$breadcrumb[] = array('navigationtitle' => $ancestor->getLabel(), 'href' => LinkHelper::getDocumentUrl($ancestor));
+					
+				}
 			}
-			else
-			{
-				$includeCurrentDocument = true;
-			}
-			$breadcrumb = $ws->getBreadcrumb($page, $includeCurrentDocument);
 		}
-		else
-		{
-			$breadcrumb = null;
-		}
-
-		if ($this->isInBackoffice() && !$breadcrumb)
-		{
-			$request->setAttribute('emptyBreadcrumb', true);
-		}
-		else
-		{
-			if (count($breadcrumb) > 0)
-			{
-				$homeMenuItem = $breadcrumb[0];
-				$this->getPage()->addLink("home", "text/html", $homeMenuItem->getUrl(), $homeMenuItem->getLabel());
-			}
-			$request->setAttribute('breadcrumb', $breadcrumb);
-		}
+		
+		$class = (count($breadcrumb) == 0) ? 'first last' : 'last';
+		$breadcrumb[] = array('navigationtitle' => $pageContext->getNavigationtitle(), 'class' => $class);
+		$request->setAttribute('breadcrumb', $breadcrumb);
 		return website_BlockView::SUCCESS;
 	}
 }
