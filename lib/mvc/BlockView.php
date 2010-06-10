d32f487a54551232a4428d812f9be264fc9882e8
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-06-10 14:29:54 +0000

    More override

--- lib/mvc/BlockView.php
+++ lib/mvc/BlockView.php
@@ -132,28 +132,8 @@ class website_BlockView
 		if ($this->templateObject === null)
 		{
 			$templateName = ucfirst($request->getModuleName()) .'-'. $this->getName($request);
-			$moduleName = $request->getModuleName();
-			$actionConfig = $request->getAttribute('configuration');
-			if ($actionConfig->getConfigurationParameter('allowthemetemplate', false))
-			{
-				list($theme, ) = explode('/', $request->getContext()->getAttribute("website_page")->getPersistentPage()->getTemplate());
-				$packageName = 'themes_' . $theme;
-				try 
-				{
-					$this->templateObject = TemplateLoader::getInstance()->setMimeContentType(K::HTML)->setPackageName($packageName)
-															->setDirectory('blocktemplates/'. $moduleName)->load($templateName);
-				}
-				catch (TemplateNotFoundException $e)
-				{
-					Framework::warn(__METHOD__ . ' : ' . $e->getMessage());
-				}
-			}
-			
-			if ($this->templateObject === null)
-			{
-				$this->templateObject = TemplateLoader::getInstance()->setMimeContentType(K::HTML)
-											->setDirectory('templates')->setPackageName( 'modules_' . $request->getModuleName())->load($templateName);
-			}
+			$this->templateObject = TemplateLoader::getInstance()->setMimeContentType(K::HTML)
+										->setDirectory('templates')->setPackageName( 'modules_' . $request->getModuleName())->load($templateName);
 		}
 		
 		$template = $this->templateObject;
