2145951901a1323da928ceb6d62f3957090f0182
Gaël PORT <gael.port@rbs.fr>
2010-06-16 07:59:52 +0000

    [FIX #14812] Traduction des pages externes.

--- persistentdocument/pageexternal.class.php
+++ persistentdocument/pageexternal.class.php
@@ -3,11 +3,8 @@
  * website_persistentdocument_pageexternal
  * @package website
  */
-class website_persistentdocument_pageexternal
-	extends website_persistentdocument_pageexternalbase
-	implements website_PublishableElement
+class website_persistentdocument_pageexternal extends website_persistentdocument_pageexternalbase implements website_PublishableElement
 {
-
 	/**
 	 * @see website_PublishableElement::getNavigationURL()
 	 * @return string
