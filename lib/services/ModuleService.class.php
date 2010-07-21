15733b91ff1e6d657e1e830d10fd89becafb7636
Gaël PORT <gael.port@rbs.fr>
2010-07-21 11:17:24 +0200

    [NEW] Added standard structure initialization mechanism, allowing to select the page template. [REFACTOR] Use the standard structure initialization mechanism to import website default structure.

--- lib/services/ModuleService.class.php
+++ lib/services/ModuleService.class.php
@@ -73,4 +73,83 @@ class website_ModuleService extends ModuleBaseService
 		}
 		return $result;
 	}
+	
+	/**
+	 * @param f_peristentdocument_PersistentDocument $container
+	 * @param string $module
+	 * @param string $pageTemplate
+	 * @param string $script
+	 */
+	public function inititalizeStructure($container, $module, $pageTemplate, $script)
+	{
+		$scriptPath = FileResolver::getInstance()->setPackageName('modules_'.$module)->setDirectory('setup')->getPath($script.'.xml');
+		if ($scriptPath === null)
+		{
+			throw new BaseException('Import script not found!', 'modules.website.bo.general.Import-script-not-found');
+		}
+		
+		$scriptContent = f_util_FileUtils::read($scriptPath);
+		$scriptContent = str_replace('"webfactory/tplTwo"', '"'.$pageTemplate.'"', $scriptContent);
+		
+		$scriptDom = new DOMDocument('1.0', 'UTF-8');
+		$scriptDom->loadXML($scriptContent);
+				
+		$ms = ModuleBaseService::getInstanceByModuleName($module);
+		$ms->updateStructureInitializationScript($container, $pageTemplate, $script, $scriptDom);
+		
+		$tmpFile = f_util_FileUtils::getTmpFile('Script_');
+		$scriptDom->save($tmpFile);
+	
+		$tm = $this->getTransactionManager();
+		try 
+		{
+			$tm->beginTransaction();			
+			$scriptReader = import_ScriptReader::getInstance();
+			Framework::info('Import Default Struct : ' . $tmpFile);
+			$scriptReader->execute($tmpFile);
+			@unlink($tmpFile);
+			$tm->commit();
+		}
+		catch (Exception $e)
+		{
+			$tm->rollBack($e);
+			@unlink($tmpFile);
+			throw $e;
+		}
+	}
+	
+	/**
+	 * @param f_peristentdocument_PersistentDocument $container
+	 * @param string $pageTemplate
+	 * @param string $script
+	 * @param DOMDocument $scriptPath
+	 */
+	public function updateStructureInitializationScript($container, $pageTemplate, $script, $scriptDom)
+	{
+		// Check container.
+		if (!$container instanceof website_persistentdocument_website)
+		{
+			throw new BaseException('Invalid website', 'modules.website.bo.actions.Invalid-website');
+		}
+		else
+		{
+			$node = TreeService::getInstance()->getInstanceByDocument($container);
+			if (count($node->getChildren('modules_website/topic')) > 0)
+			{
+				throw new BaseException('Website is not empty', 'modules.website.bo.actions.Website-is-not-empty');
+			}
+		}
+		
+		// Fix script content.
+		$xmlWebsite = $scriptDom->getElementsByTagName('website')->item(0);
+		$xmlWebsite->setAttribute('documentid', $container->getId());
+		$xmlWebsite->setAttribute('domain', $container->getDomain());
+		$xmlWebsite->setAttribute('url', $container->getUrl());
+		
+		$xmlWebsite->removeAttribute('label');
+		$xmlWebsite->removeAttribute('label-en');
+		$xmlWebsite->removeAttribute('protocol');
+		$xmlWebsite->removeAttribute('localizebypath');
+		$xmlWebsite->removeAttribute('byTag');
+	}
 }
