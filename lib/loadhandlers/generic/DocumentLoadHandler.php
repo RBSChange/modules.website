8ff039f923ef2e445694f2baa53a60676c0706f5
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-10-12 13:37:31 +0200

    [NEW] DocumentLoadHandler now permits to validate document class

--- lib/loadhandlers/generic/DocumentLoadHandler.php
+++ lib/loadhandlers/generic/DocumentLoadHandler.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This loadHandler loads a document using a parameter value and defines a request attribute with it
+ * @example use mydocId parameter value to load an instance of mymodule_persistentdocument_mydoc and set is as "mydoc" attribute <tal:block change:loadhandler="generic_DocumentLoadHandler" args="mydoc, mydocId, mymodule_persistentdocument_mydoc" />
+ */
 class generic_DocumentLoadHandler extends website_ViewLoadHandlerImpl
 {
 	/**
@@ -11,8 +15,9 @@ class generic_DocumentLoadHandler extends website_ViewLoadHandlerImpl
 		
 		if (!$request->hasAttribute($attrName))
 		{
-			$paramName = $this->getParameter(1, K::COMPONENT_ID_ACCESSOR);	
-			$request->setAttribute($attrName, $this->getDocumentParameter($paramName));
+			$paramName = $this->getParameter(1, K::COMPONENT_ID_ACCESSOR);
+			$expectedClassName = $this->getParameter(2);
+			$request->setAttribute($attrName, $this->getDocumentParameter($paramName, $expectedClassName));
 		}
 	}
 }
