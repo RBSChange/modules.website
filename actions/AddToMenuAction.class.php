1a9f7fd38fd968f14837414e3e6fbf8522b729d0
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-12-02 09:53:47 +0100

    [FIX] Correction de l'action AddToMenu

--- actions/AddToMenuAction.class.php
+++ actions/AddToMenuAction.class.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * @date Wed Jan 31 17:52:45 CET 2007
- * @author INTbonjF
- */
-class website_AddToMenuAction extends f_action_BaseAction
+class website_AddToMenuAction extends f_action_BaseJSONAction
 {
 	/**
 	 * @param Context $context
@@ -13,17 +9,21 @@ class website_AddToMenuAction extends f_action_BaseAction
 	{
 		$pagesId = $this->getDocumentIdArrayFromRequest($request);
 		$menuId  = $request->getParameter(K::DESTINATION_ID_ACCESSOR);
-		
+		$label = array("");
 		$menuObject = DocumentHelper::getDocumentInstance($menuId); 
+		$menulabel = $menuObject->getLabel();
 		foreach ($pagesId as $pageId)
 		{
 			$document = DocumentHelper::getDocumentInstance($pageId);		
 			$menuItemDoc = website_MenuitemdocumentService::getInstance()->getNewDocumentInstance();
+			$label[] = $document->getLabel();
 			$menuItemDoc->setLabel($document->getLabel());
 			$menuItemDoc->setDocument($document);
 			$menuItemDoc->save($menuObject->getId());
 		}
 
-		return self::getSuccessView();
+		return $this->sendJSON(array('message' => LocaleService::getInstance()->transBO('m.website.bo.actions.addtomenuactionsuccess', 
+			array(), 
+			array('label' => implode("\n  ", $label), 'menulabel' => $menulabel))));
 	}
 }
