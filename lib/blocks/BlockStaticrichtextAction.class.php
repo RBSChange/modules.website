12c30a03386a8afffefaba129ed2a80f88b58901
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-12-14 15:57:35 +0100

    [FIX #24351] Media localisé sans fichier

--- lib/blocks/BlockStaticrichtextAction.class.php
+++ lib/blocks/BlockStaticrichtextAction.class.php
@@ -16,6 +16,7 @@ class website_BlockStaticrichtextAction extends website_BlockAction
 		{
 			$dom = new DOMDocument("1.0", "UTF-8");
 			$elem = $dom->createElement('richtextcontent');
+			$content = preg_replace_callback('/<img\s+(.*?)\/>/i', array($this, "parseImageTagsBO"), $content);
 			$elem->appendChild($dom->createCDATASection($content));
 			$elem->setAttribute('style', 'display:none');
 			$dom->appendChild($elem);
@@ -28,4 +29,42 @@ class website_BlockStaticrichtextAction extends website_BlockAction
 		}
 	    return null;
 	}
+
+        private function parseImageTagsBO($imgMatch)
+        {
+                $matches = array();
+                $attrs = array();
+                if (preg_match_all('/\s*([\w:]*)\s*=\s*"(.*?)"/i', $imgMatch[1], $matches, PREG_SET_ORDER))
+                {
+                        foreach ($matches as $match)
+                        {
+                                $attrs[strtolower($match[1])] = isset($match[3]) ? $match[3] : $match[2];
+                        }
+                }
+                if (isset($attrs["cmpref"]))
+                {
+                        $rc = RequestContext::getInstance();
+                        $lang = (isset($attrs["lang"]) ? $attrs["lang"] : $rc->getLang());
+                        $media = DocumentHelper::getDocumentInstance($attrs["cmpref"]);
+                        $formatInfo = $attrs;
+                        if (isset($attrs['format']) && !empty($attrs['format']))
+                {
+                    list($stylesheet, $formatName) = explode('/', $attrs['format']);
+                                $formatInfo = MediaHelper::getFormatProperties($stylesheet, $formatName);
+                }
+                if (!$media->isLangAvailable($lang) || $media->getFilenameForLang($lang) === null)
+                {
+                        $lang = $media->getLang();
+                }
+                        $attrs["src"] = media_MediaService::getInstance()->generateUrl($media, $lang, $formatInfo);
+                        $imgStr = "<img";
+                        foreach ($attrs as $key => $value)
+                        {
+                                $imgStr .= " ".$key."=\"".$value."\"";
+                        }
+                        return $imgStr;
+                }
+                return "<img " . $matches[1] . "/>";
+        }
+
 }
