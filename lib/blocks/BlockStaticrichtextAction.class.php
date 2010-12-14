adc276e3129945715f58bc1ad2f534537dcf6283
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-12-14 14:46:09 +0100

    [FIX] partially localized media are not correctly rendered in page content editor

--- lib/blocks/BlockStaticrichtextAction.class.php
+++ lib/blocks/BlockStaticrichtextAction.class.php
@@ -11,9 +11,10 @@ class website_BlockStaticrichtextAction extends website_BlockAction
 	function execute($request, $response)
 	{
 		// TODO: cache for a given time & dependent on page
-	    $content = $this->getConfigurationParameter('content');
-	    if ($this->isInBackoffice())
+		$content = $this->getConfigurationParameter('content');
+		if ($this->isInBackoffice())
 		{
+			$content = preg_replace_callback('/<img\s+(.*?)\/>/i', array($this, "parseImageTagsBO"), $content);
 			$dom = new DOMDocument("1.0", "UTF-8");
 			$elem = $dom->createElement('richtextcontent');
 			$elem->appendChild($dom->createCDATASection($content));
@@ -22,10 +23,46 @@ class website_BlockStaticrichtextAction extends website_BlockAction
 			$response->write($dom->saveXML($elem));
 		}
 		else if ($content)
-    	{
-
-        	$response->write(f_util_HtmlUtils::renderHtmlFragment($content));
+		{
+			$response->write(f_util_HtmlUtils::renderHtmlFragment($content));
 		}
-	    return null;
+		return null;
+	}
+	
+	private function parseImageTagsBO($imgMatch)
+	{
+		$matches = array();
+		$attrs = array();
+		if (preg_match_all('/\s*([\w:]*)\s*=\s*"(.*?)"/i', $imgMatch[1], $matches, PREG_SET_ORDER))
+		{
+			foreach ($matches as $match)
+			{
+				$attrs[strtolower($match[1])] = isset($match[3]) ? $match[3] : $match[2];
+			}
+		}
+		if (isset($attrs["cmpref"]))
+		{	
+			$rc = RequestContext::getInstance();
+			$lang = (isset($attrs["lang"]) ? $attrs["lang"] : $rc->getLang());
+			$media = DocumentHelper::getDocumentInstance($attrs["cmpref"]);
+			$formatInfo = $attrs;
+			if (isset($attrs['format']) && !empty($attrs['format']))
+	        {
+	            list($stylesheet, $formatName) = explode('/', $attrs['format']);
+				$formatInfo = MediaHelper::getFormatProperties($stylesheet, $formatName);
+	        }
+	        if (!$media->isLangAvailable($lang) || $media->getFilenameForLang($lang) === null)
+	        {
+	        	$lang = $media->getLang();
+	        }
+			$attrs["src"] = media_MediaService::getInstance()->generateUrl($media, $lang, $formatInfo);
+			$imgStr = "<img";
+			foreach ($attrs as $key => $value)
+			{
+				$imgStr .= " ".$key."=\"".$value."\"";	
+			}
+			return $imgStr;
+		}
+		return "<img " . $matches[1] . "/>";
 	}
 }
