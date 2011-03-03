beeeeaa58d9020529d6afa0aec1f46d58d16490a
Gaël PORT <gael.port@rbs.fr>
2011-03-03 10:42:33 +0100

    [FIX #27743] Ajout de commentaire à un produit

--- lib/mvc/BlockController.php
+++ lib/mvc/BlockController.php
@@ -871,14 +871,13 @@ class website_BlockController implements f_mvc_Controller
 			$subResponse = $this->getResponse()->getWriter()->getContent();
 			array_pop($this->responseStack);
 			$this->getResponse()->getWriter()->write($subResponse);
-			$this->action = f_util_ArrayUtils::firstElement($this->actionStack);
+			$this->action = f_util_ArrayUtils::lastElement($this->actionStack);
 			$this->currentBlockId = $this->action->getBlockId();
 		}
 		else
 		{
 			$this->action = null;
 		}
-
 	}
 
 	private function popRequest()
@@ -886,7 +885,7 @@ class website_BlockController implements f_mvc_Controller
 		array_pop($this->actionRequestStack);
 		if (f_util_ArrayUtils::isNotEmpty($this->actionRequestStack))
 		{
-			$this->actionRequest = f_util_ArrayUtils::firstElement($this->actionRequestStack);
+			$this->actionRequest = f_util_ArrayUtils::lastElement($this->actionRequestStack);
 		}
 		else
 		{
