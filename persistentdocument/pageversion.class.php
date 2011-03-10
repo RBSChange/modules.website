de88f735d84ec4175bcbec134def5d39dd13dd99
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-03-10 09:44:57 +0100

    page versions pre-visualization

--- persistentdocument/pageversion.class.php
+++ persistentdocument/pageversion.class.php
@@ -28,23 +28,33 @@ class website_persistentdocument_pageversion extends website_persistentdocument_
 	{
 		if ($this->isContextLangAvailable())
 		{
+			$url = $this->isPublished() ? LinkHelper::getDocumentUrl($this) : 
+				LinkHelper::getUIActionLink("website", "BoDisplay")
+					->setQueryParameter("cmpref", $this->getId())
+					->setQueryParameter("lang", $this->getContextLang())->getUrl();
 			$data  = array('id' => $this->getId(), 
 						'lang' => $this->getContextLang(),
 						'label' => $this->getLabel(),
 						'startpublicationdate' => $this->getStartpublicationdate(),
 						'endpublicationdate' => $this->getEndpublicationdate(),
 						'publicationstatus' => $this->getPublicationstatus(),
+						'url' => $url,
 						'status' => f_Locale::translateUI(DocumentHelper::getPublicationstatusLocaleKey($this)),
 			);
 		}
 		else 
 		{
+			$url = $this->isPublished() ? LinkHelper::getDocumentUrl($this, $this->getLang()) : 
+				LinkHelper::getUIActionLink("website", "BoDisplay")
+					->setQueryParameter("cmpref", $this->getId())
+					->setQueryParameter("lang", $this->getLang())->getUrl();
 			$data  = array('id' => $this->getId(), 
 						'lang' => $this->getLang(),
 						'label' => '(' . $this->getLang() . ') ' . $this->getVoLabel(),
 						'startpublicationdate' => null,
 						'endpublicationdate' => null,
 						'publicationstatus' => null,
+						'url' => $url,
 						'status' => null,
 			);			
 		}		
