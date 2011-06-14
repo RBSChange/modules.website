187ae1635294b2ccc539191e471434141be7c2ef
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-06-14 17:46:57 +0200

    [FIX] iframe block use "width" parameter
    Conflicts:
    
    	config/blocks.xml
    	i18n/bo/blocks/iframe/de_DE.xml
    	i18n/bo/blocks/iframe/en_GB.xml
    	i18n/bo/blocks/iframe/fr_FR.xml
    	patch/lastpatch

---
+++ patch/0321/migratePageContent.php
@@ -0,0 +1,42 @@
+<?php
+$lang = $_POST['argv'][0];
+$pageIds = $_POST['argv'][1];
+
+$tm = f_persistentdocument_TransactionManager::getInstance();
+$pp = f_persistentdocument_PersistentProvider::getInstance();
+$rc = RequestContext::getInstance();
+
+$xslP = new XSLTProcessor();
+$xsl = new DOMDocument();
+$xsl->load(f_util_FileUtils::buildWebeditPath("modules/website/patch/0321/migratePageContent.xsl"));
+$xslP->importStyleSheet($xsl);
+
+try
+{
+	$tm->beginTransaction();
+	$rc->beginI18nWork($lang);
+	$query = website_PageService::getInstance()->createQuery()
+		->add(Restrictions::in("id", $pageIds));
+	$pages = $query->find();
+	$updatedCount = 0;
+	foreach ($pages as $page)
+	{
+		$contentDoc = new DOMDocument();
+		$contentDoc->loadXML($page->getContent());
+		$newContent = $xslP->transformToXML($contentDoc);
+		$page->setContent($newContent);
+		if ($page->isModified())
+		{
+			$pp->updateDocument($page);
+			$updatedCount++;
+		}
+	}
+	$tm->commit();
+	$rc->endI18nWork();
+	echo $updatedCount;
+}
+catch (Exception $e)
+{
+	$tm->rollBack($e);
+	echo "ERROR: " . $e->getMessage();
+}
