94ab0b6cb41b727965e696392659a070d93a8b92
Gaël PORT <gael.port@rbs.fr>
2011-08-01 17:19:35 +0200

    [REFACTOR] Move classes from framework/mvc to modules/website/lib/mvc

---
+++ lib/mvc/HTTPSession.php
@@ -0,0 +1,77 @@
+<?php
+interface f_mvc_Session
+{
+	/**
+	 * @param String $name
+	 * @param mixed $value
+	 */
+	function setAttribute($name, $value);
+	
+	/**
+	 * @param String $name
+	 * @return mixed
+	 */
+	function getAttribute($name);
+	
+	/**
+	 * @return array<String, mixed>
+	 */
+	function getAttributes();
+	
+	/**
+	 * @param String $name
+	 */
+	function removeAttribute($name);
+}
+
+class f_mvc_HTTPSession implements f_mvc_Session 
+{
+	/**
+	 * @param String $name
+	 * @param mixed $value
+	 */
+	function setAttribute($name, $value)
+	{
+		$_SESSION[$name] = $value;
+	}
+
+	/**
+	 * @param String $name
+	 * @return mixed
+	 */
+	function getAttribute($name)
+	{
+		if (isset($_SESSION[$name]))
+		{
+			return $_SESSION[$name];
+		}
+		return null;
+	}
+
+	/**
+	 * @param String $name
+	 */
+	function removeAttribute($name)
+	{
+		unset($_SESSION[$name]);
+	}
+	
+
+	/**
+	 * @return array<String, mixed>
+	 */
+	function getAttributes()
+	{
+		return $_SESSION;
+	}
+
+	/**
+	 * @param String $name
+	 * @return Boolean
+	 */
+	function hasAttribute($name)
+	{
+		return array_key_exists($name, $_SESSION);
+	}
+
+}
