ff822ddc068e7622a8991f2f904cc4e2e7e0706d
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-04 09:22:10 +0200

    [UPDATE] Mise à jour dtd xhtml fragment [UPDATE] Correction template blank action

--- lib/formatter/Xhtml.php
+++ lib/formatter/Xhtml.php
@@ -3,7 +3,16 @@ class formatter_Xhtml
 {
 	public function format($xhtml)
 	{
-		$xml = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "file://'. WEBEDIT_HOME .'/modules/website/lib/fckeditor/xhtml1DTD/xhtml1-transitional.dtd"><xhtml xmlns="http://www.w3.org/1999/xhtml" xmlns:change="http://www.rbs.fr/change/1.0/schema"><richtextcontent>' . $xhtml .'</richtextcontent></xhtml>';
+		if (DIRECTORY_SEPARATOR !== '/')
+		{
+			$dtdPath = 'file:///' . str_replace(array(DIRECTORY_SEPARATOR, ' '), array('/', '%20'), realpath(PROJECT_HOME .'/framework/f_web/dtd/xhtml1-transitional.dtd'));
+		}
+		else
+		{
+			$dtdPath = 'file://' . str_replace(' ', '%20', realpath(PROJECT_HOME .'/framework/f_web/dtd/xhtml1-transitional.dtd'));
+		}
+		
+		$xml = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "'.$dtdPath.'"><xhtml xmlns="http://www.w3.org/1999/xhtml" xmlns:change="http://www.rbs.fr/change/1.0/schema"><richtextcontent>' . $xhtml .'</richtextcontent></xhtml>';
 		$doc = new DOMDocument();
 		$doc->substituteEntities = false;
 		$doc->resolveExternals = true;
