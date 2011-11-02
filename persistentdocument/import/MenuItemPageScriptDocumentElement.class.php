3ff25de6a5c825afd535d635f933b38277af9f0e
Gaël PORT <gael.port@rbs.fr>
2011-11-02 14:44:20 +0100

    [FIX #46185] Doublons lors de l'import de menuitems

--- persistentdocument/import/MenuItemPageScriptDocumentElement.class.php
+++ persistentdocument/import/MenuItemPageScriptDocumentElement.class.php
@@ -1,42 +1,51 @@
 <?php
+/**
+ * @deprecated use website_MenuitemdocumentScriptDocumentElement instead
+ */
 class website_MenuItemPageScriptDocumentElement extends import_ScriptDocumentElement
 {
-    /**
-     * @return f_persistentdocument_PersistentDocument
-     */
-    protected function initPersistentDocument()
-    {
-        return website_MenuitemdocumentService::getInstance()->getNewDocumentInstance();
-    }
-    
+	/**
+	 * @deprecated
+	 */
+	protected function initPersistentDocument()
+	{
+		return website_MenuitemdocumentService::getInstance()->getNewDocumentInstance();
+	}
+
+	/**
+	 * @deprecated
+	 */
 	protected function getDocumentType()
-    {
+	{
 		return "modules_website/menuitemdocument";
-    }
+	}
 
-    protected function getDocumentProperties ()
-    {
-        $properties = parent::getDocumentProperties();
-        if (isset($properties['pageid']))
-        {
-            $scriptElement = $this->script->getElementById($properties['pageid']);
-            unset($properties['pageid']);
+	/**
+	 * @deprecated
+	 */
+	protected function getDocumentProperties ()
+	{
+		$properties = parent::getDocumentProperties();
+		if (isset($properties['pageid']))
+		{
+			$scriptElement = $this->script->getElementById($properties['pageid']);
+			unset($properties['pageid']);
 
-            $document = $scriptElement->getPersistentDocument();
-            $properties['document'] = $document;
-            $properties['label'] = $document->getLabel();
-        }
-        elseif (isset($properties['pagetag']))
-        {
-            $documents = TagService::getInstance()->getDocumentsByTag($properties['pagetag']);
-            unset($properties['pagetag']);
-            if (count($documents) > 0)
-            {
-                $properties['document'] = $documents[0];
-                $properties['label'] = $documents[0]->getLabel();
-            }
-        }
+			$document = $scriptElement->getPersistentDocument();
+			$properties['document'] = $document;
+			$properties['label'] = $document->getLabel();
+		}
+		elseif (isset($properties['pagetag']))
+		{
+			$documents = TagService::getInstance()->getDocumentsByTag($properties['pagetag']);
+			unset($properties['pagetag']);
+			if (count($documents) > 0)
+			{
+				$properties['document'] = $documents[0];
+				$properties['label'] = $documents[0]->getLabel();
+			}
+		}
 
-        return $properties;
-    }
+		return $properties;
+	}
 }
--- persistentdocument/import/MenuitemdocumentScriptDocumentElement.class.php
+++ persistentdocument/import/MenuitemdocumentScriptDocumentElement.class.php
@@ -5,11 +5,20 @@
  */
 class website_MenuitemdocumentScriptDocumentElement extends import_ScriptDocumentElement
 {
-    /**
-     * @return website_persistentdocument_menuitemdocument
-     */
-    protected function initPersistentDocument()
-    {
-    	return website_MenuitemdocumentService::getInstance()->getNewDocumentInstance();
-    }
+	/**
+	 * @return website_persistentdocument_menuitemdocument
+	 */
+	protected function initPersistentDocument()
+	{
+		$doc = $this->getComputedAttribute('document', true);
+		$menu = $this->getParentDocument()->getPersistentDocument();
+		$mids = website_MenuitemdocumentService::getInstance();
+		$item = $mids->createQuery()->add(Restrictions::eq('document', $doc))->add(Restrictions::eq('menu', $menu))->findUnique();
+		if ($item === null)
+		{
+			$item = website_MenuitemdocumentService::getInstance()->getNewDocumentInstance();
+			$item->setDocument($doc);
+		}
+		return $item;	
+	}
 }
