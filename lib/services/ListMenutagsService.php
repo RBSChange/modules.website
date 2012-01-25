5663200acc3fe7059f6dfc6842bbb7bd8d004e9d
Gaël PORT <gael.port@rbs.fr>
2012-01-25 17:27:46 +0100

    [NEW] Add visual property on topics. [NEW] Add modules_website/menutags dynamic list.

---
+++ lib/services/ListMenutagsService.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * website_ListMenutagsService
+ * @package modules.website.lib.services
+ */
+class website_ListMenutagsService extends BaseService implements list_ListItemsService
+{
+	/**
+	 * @var website_ListMenutagsService
+	 */
+	private static $instance;
+
+	/**
+	 * @return website_ListMenutagsService
+	 */
+	public static function getInstance()
+	{
+		if (self::$instance === null)
+		{
+			self::$instance = self::getServiceClassInstance(get_class());
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * @see list_persistentdocument_dynamiclist::getItems()
+	 * @return list_Item[]
+	 */
+	public final function getItems()
+	{
+		$items = array();
+		foreach (TagService::getInstance()->getAvailableTagsInfoByModel('modules_website/menu') as $tag => $tagInfos)
+		{
+			if ($tagInfos['labeli18n'])
+			{
+				$label = LocaleService::getInstance()->transBO($tagInfos['labeli18n'], array('ucf'));
+			}
+			else
+			{
+				$label = f_Locale::translate($tagInfos['label']);
+			}
+			$items[] = new list_Item($label, $tag);
+		}
+		Framework::fatal(__METHOD__ . ' ' . var_export($items, true));
+		return $items;
+	}
+
+	/**
+	 * @var Array
+	 */
+	private $parameters = array();
+	
+	/**
+	 * @see list_persistentdocument_dynamiclist::getListService()
+	 * @param array $parameters
+	 */
+	public function setParameters($parameters)
+	{
+		$this->parameters = $parameters;
+	}
+}
---
+++ patch/0366/install.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * website_patch_0366
+ * @package modules.website
+ */
+class website_patch_0366 extends patch_BasePatch
+{
+	/**
+	 * Entry point of the patch execution.
+	 */
+	public function execute()
+	{
+		$newPath = f_util_FileUtils::buildWebeditPath('modules/website/persistentdocument/topic.xml');
+		$newModel = generator_PersistentModel::loadModelFromString(f_util_FileUtils::read($newPath), 'website', 'topic');
+		$newProp = $newModel->getPropertyByName('visual');
+		f_persistentdocument_PersistentProvider::getInstance()->addProperty('website', 'topic', $newProp);
+		$this->execChangeCommand('compile-db-schema');
+		
+		$this->executeLocalXmlScript('list-menutags.xml');
+	}
+}
