c8755479d51fcb9eaa4f0d5cac2db68a1a733878
Gaël PORT <gael.port@rbs.fr>
2012-02-21 16:31:48 +0100

    [REFACTOR] Move unused code to compatibilityos.

--- actions/EditContentAction.class.php
+++ actions/EditContentAction.class.php
@@ -1,34 +1,21 @@
 <?php
 class website_EditContentAction extends f_action_BaseAction
 {
-    const FORCE_PAGE_RESET = "force_page_reset";
-    
-    /**
+	const FORCE_PAGE_RESET = 'force_page_reset';
+	
+	/**
      * @param Context $context
      * @param Request $request
      */
-    public function _execute ($context, $request)
-    {
-        try
-        {
-            $document = DocumentHelper::getCorrection($this->getDocumentInstanceFromRequest($request));
-            $ds = $document->getDocumentService();
-            if (!$document->getContent() || $request->hasParameter(self::FORCE_PAGE_RESET))
-            {
-                $ds->setDefaultContent($document);
-            }
-            $request->setAttribute('document', $document);
-        } 
-        catch (Exception $e)
-        {
-            Framework::exception($e);
-            $request->setAttribute('error', $e->getMessage());
-            if (isset($document) && $document)
-            {
-                $request->setAttribute('document', $document);
-            }
-            return View::ERROR;
-        }
-        return View::SUCCESS;
-    }
+	public function _execute($context, $request)
+	{
+		$document = DocumentHelper::getCorrection($this->getDocumentInstanceFromRequest($request));
+		$ds = $document->getDocumentService();
+		if (!$document->getContent() || $request->hasParameter(self::FORCE_PAGE_RESET))
+		{
+			$ds->setDefaultContent($document);
+		}
+		$request->setAttribute('document', $document);
+		return View::SUCCESS;
+	}
 }
