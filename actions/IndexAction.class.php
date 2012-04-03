629537715eca1c9ff77550383528cc8da86a8514
Gaël PORT <gael.port@rbs.fr>
2012-04-03 10:48:06 +0200

    [FIX #56181] URL de la page d'accueil d'un site et localisation par chemin

--- actions/IndexAction.class.php
+++ actions/IndexAction.class.php
@@ -13,13 +13,25 @@ class website_IndexAction extends f_action_BaseAction
 			website_UrlRewritingService::getInstance()->initCurrrentWebsite($host);
 			$ws = website_WebsiteModuleService::getInstance();
 			$website = $ws->getCurrentWebsite();
-			$homePage = $ws->getIndexPage($website, false);
-			if ($homePage  === null || !$homePage->isPublished())
+			if ($website->getLocalizebypath())
 			{
-				throw new Exception('Website has no published home page');
+				if (Framework::isInfoEnabled())
+				{
+					Framework::info(__METHOD__ . ' redirect on VO homepage');
+				}
+				$request->setParameter('location', LinkHelper::getDocumentUrl($website, $website->getLang()));
+				$context->getController()->forward('website', 'Redirect');
 			}
-			$request->setParameter(K::PAGE_REF_ACCESSOR, $homePage->getId());
-			$context->getController()->forward('website', 'Display');
+			else
+			{		
+				$homePage = $ws->getIndexPage($website, false);
+				if ($homePage  === null || !$homePage->isPublished())
+				{
+					throw new Exception('Website has no published home page');
+				}
+				$request->setParameter(K::PAGE_REF_ACCESSOR, $homePage->getId());
+				$context->getController()->forward('website', 'Display');
+			}
 		} 
 		catch (Exception $e) 
 		{
