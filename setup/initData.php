c8d55044d6019a400a9564f6ce9f089c74e76f0c
Gaël PORT <gael.port@rbs.fr>
2012-04-05 12:32:38 +0200

    [REFACTOR] Simplify module initialization.

--- setup/initData.php
+++ setup/initData.php
@@ -3,20 +3,16 @@ class website_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-
-		
 		$this->addProjectConfigurationEntry('tal/prefix/alternateclass', 'website_TalesAlternateClass');
 		$this->addProjectConfigurationEntry('tal/prefix/url', 'website_TalesUrl');
 		$this->addProjectConfigurationEntry('tal/prefix/actionurl', 'website_TalesUrl');
 		$this->addProjectConfigurationEntry('tal/prefix/currenturl', 'website_TalesUrl');
 		
-		//Ajout des liens symboliques pour l'utilisation de fckeditor en front office
+		// Make symbolic links for fckeditor.
 		$repositoryPath = f_util_FileUtils::buildWebeditPath('libs', 'fckeditor');
-		$browserPath = f_util_FileUtils::buildWebeditPath('modules', 'website',  'lib', 'fckeditor', 'browser');
+		$browserPath = f_util_FileUtils::buildWebeditPath('modules', 'website', 'lib', 'fckeditor', 'browser');
 		$webappFckPath = f_util_FileUtils::buildDocumentRootPath('fckeditor');
 		$webappBrowserPath = f_util_FileUtils::buildDocumentRootPath('fckeditorbrowser');
-		
-	
 		
 		if (is_link($webappFckPath))
 		{
@@ -28,21 +24,14 @@ class website_Setup extends object_InitDataSetup
 		if (is_link($webappBrowserPath))
 		{
 			@unlink($webappBrowserPath);
-		}		
+		}
 		$this->addInfo("Creating symbolic link for fckeditorbrowser...");
 		f_util_FileUtils::symlink($browserPath, $webappBrowserPath);
-
-		$this->executeModuleScript('init.xml');	
-		$this->executeModuleScript('markers.xml');	
 		
-		// Fix #721: import the new workflow first, so it will be activated by
-		// default, instead of the old one.
+		// Import datas (lists, workflows, etc).
+		$this->executeModuleScript('init.xml');
 		$this->executeModuleScript('workflow2.xml');
 		
-		//$this->executeModuleScript('workflow1.xml');		
-		$this->executeModuleScript('iframe.xml');	
-		
-
 		$tasks = task_PlannedtaskService::getInstance()->getBySystemtaskclassname('website_CleanCSSAndJSCacheTask');
 		if (count($tasks) == 0)
 		{
