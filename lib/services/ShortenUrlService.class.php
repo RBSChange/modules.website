97643325144632ec69881d4ae67bd66581fb7038
Gaël PORT <gael.port@rbs.fr>
2012-09-21 12:21:05 +0200

    [FIX] No more URL shorting by default.

--- lib/services/ShortenUrlService.class.php
+++ lib/services/ShortenUrlService.class.php
@@ -6,21 +6,11 @@
 class website_ShortenUrlService extends change_BaseService
 {
 	/**
-	 * @var Zend_Service_ShortUrl_AbstractShortener 
-	 */
-	private $shortenerInstance;
-	
-	/**
 	 * @param string $url
 	 * @return string
 	 */
 	public function shortenUrl($url)
 	{
-		if ($this->shortenerInstance == null)
-		{
-			$this->shortenerInstance = f_util_ClassUtils::newInstance(Framework::getConfiguration('modules/website/shortenerClassName'));
-		}
-		$this->shortenerInstance->setHttpClient(change_HttpClientService::getInstance()->getNewHttpClient());
-		return $this->shortenerInstance->shorten($url);
+		return $url;
 	}
 }
