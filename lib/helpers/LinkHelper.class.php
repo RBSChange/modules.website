99ceda72671610df9c87f7513e4f70498b54a91d
Gaël PORT <gael.port@rbs.fr>
2013-06-24 14:16:06 +0200

    [DEPRECATE] Method getUIChromeRessourceLink

--- lib/helpers/LinkHelper.class.php
+++ lib/helpers/LinkHelper.class.php
@@ -109,8 +109,7 @@ class LinkHelper
 	}
 	
 	/**
-	 * @param string $ressourceName
-	 * @return f_web_ResourceLink
+	 * @deprecated 
 	 */
 	public static function getUIChromeRessourceLink($ressourceName)
 	{
